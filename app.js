2bda6c915171baba6bd8a76b90dd3ec8e4a6abdf
DebojytiDuttaChoudhury <debojyti.work@gmail.com>
2026-02-04 03:25:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const supabaseUrl = "https://retxvksxtcedlewjxkmh.supabase.co";
+const supabaseKey = "sb_publishable_zInp5lVCLC8u4EBFdwYGuA_0bC-joaV";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
